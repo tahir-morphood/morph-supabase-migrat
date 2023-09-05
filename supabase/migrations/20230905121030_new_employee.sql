acad8e34172339ac2fa9e14e0818130468933af3
tahir-morphood <tahir@morphood.com>
2023-09-05 17:33:47 +0500

    newnenw

--- supabase/migrations/20230905121030_new_employee.sql
+++ supabase/migrations/20230905121030_new_employee.sql
@@ -1,7 +1,3 @@
-alter table "public"."employees" add column "des" text;
-
-alter table "public"."employees" add column "hellocolumn" text;
-
 alter table "public"."employees" add column "please" text;
 
 
