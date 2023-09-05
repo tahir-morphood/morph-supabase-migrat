7492f6c56d822b3026dbd6a3d65afa2c66eb055c
tahir-morphood <tahir@morphood.com>
2023-09-05 17:11:46 +0500

    please column add just

---
+++ supabase/migrations/20230905121030_new_employee.sql
@@ -0,0 +1,7 @@
+alter table "public"."employees" add column "des" text;
+
+alter table "public"."employees" add column "hellocolumn" text;
+
+alter table "public"."employees" add column "please" text;
+
+
